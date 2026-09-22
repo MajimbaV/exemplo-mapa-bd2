49cb636c7706ce2cf0bd7853b641ce953cfab45f
Francisco Everton Maciel Rodrigues <202512010018@ad.ifpb.edu.br>
2026-09-22 16:42:53 -0300

    fix: Corrige erros do merge

--- back-end/index.js
+++ back-end/index.js
@@ -17,21 +17,12 @@ const client = await new Client({
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE
 }).connect()
-<<<<<<< HEAD
- 
-
-
-app.get('/municipios/:codigo', async (req, res) => {
-  const geoJson = await client.query('SELECT ST_AsGeoJson(geom) FROM municipios WHERE id = $1', [req.params.codigo]);
-  res.send(geoJson.rows[0]);
-=======
 
 app.get('/municipios/:codigo', async (req, res) => {
   const geojson = await client.query(
     'SELECT ST_AsGeoJSON(geom) FROM municipios WHERE id = $1', [req.params.codigo]
   )
   res.json(geojson.rows[0]);
->>>>>>> 77b68d98acf959ffab3abcb05fff52d09e57fb9d
 });
 
 app.listen(port, () => {
